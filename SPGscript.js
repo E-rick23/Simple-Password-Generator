3c265923cf6932a51cb25b4fab93a721e7d4f332
e-rick23 <erickmarques63@gmail.com>
2022-10-16 14:46:05 -0300

    Updated PWgen.html and SPGscript.js

--- SPGscript.js
+++ SPGscript.js
@@ -1,9 +1,10 @@
 const flags = {
-    numbers: false,
+    uppercase: false,
     numbers: false,
     symbols: false,
-    lenght: 14
+    length: 14
 }
+
 const selectors = {
     copy: 'copy',
     checkbox: 'checkbox',
@@ -12,28 +13,31 @@ const selectors = {
     sliderValue: document.querySelector('.value'),
     input: document.querySelector('input[type="text"]')
 }
+
 const generatePassword = () => {
-    const defaultchar = 'zyxwvutsrqponmlkjihgfedcba' //They're backwards because yes. :)
+    const defaultChar = 'zyxwvutsrqponmlkjihgfedcba' //They're backwards because yes. :)
     //By default, the generator starts with the alphabet in lowercase.
     const characters = {
-        uppercase: defaultchar.toUpperCase(), //This simply changes the defaultchar value to uppercase.
+        uppercase: defaultChar.toUpperCase(),  //This simply changes the defaultchar value to uppercase.
         numbers: '9876543210', //Again, backwards because yes. :)
         symbols: '!@#$%^&*~' //Some symbols for password generation.
     }
+
     const charList = [
-        defaultchar,
+        defaultChar,
         ...flags.uppercase ? characters.uppercase : [],
         ...flags.numbers ? characters.numbers : [],
         ...flags.symbols ? characters.symbols : []
     ].join('')
 
     return Array.from({ length: flags.length }, () => Math.floor(Math.random() * charList.length))
-    .map(number => charList[number])
-    .join('')
+        .map(number => charList[number])
+        .join('')
 }
+
 document.querySelector('#app').addEventListener('click', event => {
     switch (event.target.dataset.jsSelector) {
-        //Event Listener for the copy event.
+        // Event listener for copy
         case selectors.copy:
             const dummy = document.createElement('textarea')
 
@@ -46,20 +50,21 @@ document.querySelector('#app').addEventListener('click', event => {
             document.body.removeChild(dummy)
         break;
 
-        //Event Listener for the checkboxes.
+        // Event listeners for checkboxes
         case selectors.checkbox:
             flags[event.target.control.id] = !event.target.control.checked
             //This make sure the flags that are based on whether a checkbox is checked or not are properly updated.
         break;
 
-        //Event Listener for the slider.
+        // Event listeners for slider
         case selectors.slider:
             const value = event.target.valueAsNumber //This make sure we are able to get the value of the slider thanks to the valueAsNumber attibute.
 
             selectors.sliderValue.innerText = value
-            flags.lenght = value
+            flags.length = value
         break;
-        //Event Listener for the generate button.
+
+        // Event listener for generate button
         case selectors.button:
             selectors.input.value = generatePassword()
         break;
